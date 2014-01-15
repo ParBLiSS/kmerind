a6a1ffa542346d5ed21ef72ef548e3baccd2735f
tcpan <tpan7@gatech.edu>
2014-01-15 12:13:54 -0500

    WIP: test for generating the index values using serial, omp, and simd

---
+++ src/index/indexing.cpp
@@ -0,0 +1,16 @@
+/**
+ * indexing.cpp
+ *
+ *  Created on: Jan 15, 2014
+ *      Author: tpan
+ *
+ *  Description:  testing code to try different ways of generating the index key from a collection of strings
+ *
+ */
+
+const int K = 23;
+const char BITS = 2;
+
+/* TODO  finish this.
+ *
+ */
