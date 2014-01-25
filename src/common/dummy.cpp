69ad42c5e4ff91b2afe29331614a503df7eab786
r4d2 <patrick.flick@gmail.com>
2014-01-24 20:09:54 -0500

    code coverage

--- src/common/dummy.cpp
+++ src/common/dummy.cpp
@@ -4,5 +4,11 @@
 
 int getSomeNumber(int x, int y)
 {
+    int z = x*y;
+    // some random special condition (not covered by tests)
+    if (z == -13)
+    {
+        z = 0;
+    }
     return x*y;
 }
