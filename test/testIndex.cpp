0f17143b007bb5211d84577fe6b9ceafce209071
tcpan <tpan7@gatech.edu>
2014-02-26 17:35:26 -0500

    WIP: generating kmer quality score

--- test/testIndex.cpp
+++ test/testIndex.cpp
@@ -30,6 +30,7 @@
 #include <string.h>
 #include <vector>
 #include <bitset>
+#include <cmath>
 
 #include "common/alphabets.hpp"
 #include "common/AlphabetTraits.hpp"
@@ -41,12 +42,15 @@ struct generate_kmers {
     TO forward;
     TO reverse;
     TO xored;
+    TO xoredRecoverable;
+
     static constexpr BitSizeType nBits = bliss::AlphabetTraits<ALPHABET>::getBitsPerChar();
     static constexpr BitSizeType shift = bliss::AlphabetTraits<ALPHABET>::getBitsPerChar() * (K - 1);
     static constexpr AlphabetSizeType max = bliss::AlphabetTraits<ALPHABET>::getSize() - 1;
 
     static constexpr int word_size = sizeof(TO) * 8;
     static constexpr TO mask_reverse =  ~(static_cast<TO>(0))  >> (word_size - shift - nBits);
+    static constexpr TO mask_lower_half =  ~(static_cast<TO>(0))  >> (word_size - nBits * (K + 1) / 2);
 
     generate_kmers() : forward()
     { }
@@ -64,23 +68,147 @@ struct generate_kmers {
       xored = forward ^ reverse;
 //      std::cout << "kmer:\t" << std::bitset<word_size>(forward) << std::endl << "\t" << std::bitset<word_size>(reverse) << std::endl;
 //      std::cout << "\t" << std::bitset<word_size>(xored) << std::endl;
+
+      xoredRecoverable = (xored & ~mask_lower_half) | (forward & mask_lower_half);
+
       ++iter;
       return 1;
     }
 
     TO operator()() {
-      return xored;
+      return xoredRecoverable;
     }
-
-//    TO operator()(CharType c) {
-//      forward >>= nBits;
-//      forward |= (static_cast<TO>(ALPHABET::FROM_ASCII[c]) << shift);
-//      return forward;
-//    }
-
 
 };
 
+/**
+ * compute kmer quality based on phred quality score.
+ *
+ * Q = -10 log_10 P
+ *
+ * where P is probability of error.
+ *
+ * probability of kmer being correct is: (1-p_1)(1-p_2)...(1-p_k)
+ * probability of kmer being incorrect then is 1 - (p(correct...))
+ *
+ * Phrad adds the Phred score, which amounts to p_1 * p_2 * ... * p_k because of the logarithm.
+ *  (probability that all bases in the sequence are incorrect.)
+ * Quake is computes a kmer score based on probability in the phred score.  not clear exactly how.
+ *
+ * value range is in ascii from !(33) to ~(126), using sanger encoding.
+ *
+ * // now computing k-correct with approximation gives
+ * //    1 - sum_1..k(p_i) + 1/2 sum_1..k sum_1..k (p_i)(p_j) - 3rd order term + 4th order term ...
+ * //
+ * //   p(incorrect) is sum_1..k(p_i) - 1/2 sum_1..k sum_1..k (p_i)(p_j) + 3rd order term - 4th order term ...
+ * //
+ * // =====>>>  max_1..k(p_i) < p(incorrect) < sum_1..k(p_i).
+ * //
+ * // approximate with linear terms.  (i.e. probability of 2 or more bases being called incorrectly is low).  bound is not that tight.
+ * //
+ * // =====>>>  max_1..k(p_i) < p(
+ * //
+ * // max_1..k(p_i) ~= min_1..k(q_i)
+ *
+ *
+ *  each term in (1-p_i) is (1- 10^(q/-10)) = (1 - e ^ ((log 10)(q/-10)))
+ *  accumulate using sum(log(term))., expand using e^(sum(log(term))), calculate -10 log_10(1-cumulative).
+ *
+ */
+template<typename ENCODING, typename Iterator, typename TO, int K >
+struct generate_qual {
+    typedef typename std::iterator_traits<Iterator>::value_type TI;
+
+    TO value;
+    TO internal;
+    TO terms[K];
+    int pos;
+    int zeroCount;
+
+    static constexpr TO logE_10 = std::log(10);
+    static_assert(std::is_floating_point<TO>::value, "generate_qual output needs to be floating point type");
+
+    generate_qual() : value(1), pos(0)
+    {
+      for (int i = 0; i < K; ++i)
+      {
+        terms[i] = 0.0;
+      }
+      zeroCount = K;
+    }
+
+    TO compute(TI v) {
+      if (v == 0)
+        return 0.0;   // probability of being correct
+      else
+        return 1.0 - std::exp(static_cast<TO>(v) / -10.0f * logE_10);  // prob of being correct.
+    }
+
+    size_t operator()(Iterator &iter) {
+
+        // drop the old value
+      TO oldval = terms[pos];
+
+      // add the new value,       // update the position  - circular queue
+      TO newval = compute(*iter - 33);  // this is for Sanger encoding.
+      terms[pos] = newval;
+      pos = (pos + 1) % K;
+
+      // save the old zero count.
+      int oldZeroCount = zeroCount;
+
+
+      // update the zero count
+      if (newval == 0.0) {
+        printf("ZERO!\n");
+        ++zeroCount;
+      }
+      if (oldval == 0.0) {
+        --zeroCount;
+      }
+
+      // if any is zero, then return 0.
+      if (zeroCount > 0) {
+        internal = 0.0;
+        value = 0.0;
+      }
+      else
+      {
+        // else there is no zero,
+
+        if (oldZeroCount == 1) {
+          printf("HAD ZEROS!\n");
+          // removed a zero.  so recalculate.
+          internal = 0.0;
+          for (int i = 0; i < K; ++i) {
+            internal += std::log(terms[i]);
+          }
+        } else {
+          // there was not a zero.  so update.
+          internal = internal + std::log(newval) - std::log(oldval);
+        }
+
+        TO v = std::exp(internal);  // prob of being correct.
+        if (v == 1.0) {
+          value = 1000;
+          printf("confident kmer!\n");
+        } else {
+          value = -10.0 * std::log10(1.0 - std::exp(internal));
+        }
+      }
+
+//      printf("qual %d oldval = %f, newval = %f, internal = %f, output val = %f\n", *iter, oldval, newval, internal, value);
+
+      // move the iterator.
+      ++iter;
+      return value;
+    }
+
+    TO operator()() {
+      return value;
+    }
+
+};
 
 
 
@@ -155,11 +283,19 @@ int main(int argc, char* argv[]) {
     op_type kmer_op;
     typedef bliss::iterator::buffered_transform_iterator<op_type, char*> read_iter_type;
 
+    struct SANGER {};
+    typedef generate_qual< SANGER, char*, double, 21>  qual_op_type;
+    qual_op_type qual_op;
+    typedef bliss::iterator::buffered_transform_iterator<qual_op_type, char*> qual_iter_type;
+
+
     // transform and generate kmers
     std::vector<uint64_t> kmers;
+    std::vector<double> quals;
     for (; fastq_start != fastq_end; ++fastq_start) {
       read_iter_type start((*fastq_start).seq, kmer_op);
       read_iter_type end((*fastq_start).seq_end, kmer_op);
+
 
       int i = -1;
       // NOTE: need to call *start to actually evaluate.  question is whether ++ should be doing computation.
@@ -169,16 +305,32 @@ int main(int argc, char* argv[]) {
         kmers.push_back(*start);
       }
 
+      qual_iter_type qstart((*fastq_start).qual, qual_op);
+      qual_iter_type qend((*fastq_start).qual_end, qual_op);
+
+
+      i = -1;
+      // NOTE: need to call *start to actually evaluate.  question is whether ++ should be doing computation.
+      for (; qstart != qend; ++qstart) {
+        ++i;
+        if (i < 20) continue;
+        quals.push_back(*qstart);
+      }
+
     }
     t2 = std::chrono::high_resolution_clock::now();
     time_span3 = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
     INFO("kmer generation " << "elapsed time: " << time_span3.count() << "s.");
 
-    int i = 0;
-    for (auto kmer : kmers) {
-      std::cout << i << " " << std::bitset<64>(kmer) << std::endl;
-      ++i;
+    for (size_t i = 0; i < kmers.size(); ++i) {
+      uint64_t kmer = kmers[i];
+      double qual = quals[i];
+
+
+      std::cout << i << " qual: " << qual << " kmer " << std::bitset<64>(kmer) << std::endl;
+
     }
+
 
 
   }
