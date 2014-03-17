4dd3cdef5cdc13883024b2adfd40d33889ae3fa0
tcpan <tpan7@gatech.edu>
2014-03-17 17:59:43 -0400

    WIP: threading and MPI data movement

--- test/test_threads.cpp
+++ test/test_threads.cpp
@@ -12,7 +12,9 @@
 
 #include <iostream>
 #include <thread>
+
 #include <cstdio>
+#include <cmath>
 
 #include "mpi.h"
 #include "omp.h"
@@ -22,7 +24,224 @@
 
 #include "iterators/range.hpp"
 #include "io/fastq_loader.hpp"
-#include "io/fastq_iterator.hpp"
+
+#include "common/alphabets.hpp"
+#include "common/AlphabetTraits.hpp"
+#include "iterators/buffered_transform_iterator.hpp"
+
+
+template<typename ALPHABET, typename Iterator, typename TO, int K>
+struct generate_kmers
+{
+    TO forward;
+    TO reverse;
+    TO xored;
+    TO xoredRecoverable;
+
+    static constexpr BitSizeType nBits =
+        bliss::AlphabetTraits<ALPHABET>::getBitsPerChar();
+    static constexpr BitSizeType shift =
+        bliss::AlphabetTraits<ALPHABET>::getBitsPerChar() * (K - 1);
+    static constexpr AlphabetSizeType max =
+        bliss::AlphabetTraits<ALPHABET>::getSize() - 1;
+
+    static constexpr int word_size = sizeof(TO) * 8;
+    static constexpr TO mask_reverse = ~(static_cast<TO>(0))
+        >> (word_size - shift - nBits);
+    static constexpr TO mask_lower_half = ~(static_cast<TO>(0))
+        >> (word_size - nBits * (K + 1) / 2);
+
+    generate_kmers()
+        : forward()
+    {
+    }
+
+    size_t operator()(Iterator &iter)
+    {
+      char val = ALPHABET::FROM_ASCII[static_cast<size_t>(*iter)];
+      forward >>= nBits;
+      forward |= (static_cast<TO>(val) << shift);
+
+      char complement = max - val;
+      reverse <<= nBits;
+      reverse |= static_cast<TO>(complement);
+      reverse &= mask_reverse;
+
+      xored = forward ^ reverse;
+//      std::cout << "kmer:\t" << std::bitset<word_size>(forward) << std::endl << "\t" << std::bitset<word_size>(reverse) << std::endl;
+//      std::cout << "\t" << std::bitset<word_size>(xored) << std::endl;
+
+      xoredRecoverable = (xored & ~mask_lower_half)
+          | (forward & mask_lower_half);
+
+      ++iter;
+      return 1;
+    }
+
+    TO operator()()
+    {
+      return xoredRecoverable;
+    }
+
+};
+
+/**
+ * compute kmer quality based on phred quality score.
+ *
+ * Q = -10 log_10 P
+ *
+ * where P is probability of error.
+ *
+ * probability of kmer being correct is: (1-p_1)(1-p_2)...(1-p_k)
+ * probability of kmer being incorrect then is 1 - (p(correct...))
+ *
+ * Phrad adds the Phred score, which amounts to p_1 * p_2 * ... * p_k because of the logarithm.
+ *  (probability that all bases in the sequence are incorrect.)
+ * Quake is computes a kmer score based on probability in the phred score.  not clear exactly how.
+ *
+ * value range is in ascii from !(33) to ~(126), using sanger encoding.
+ *
+ * // now computing k-correct with approximation gives
+ * //    1 - sum_1..k(p_i) + 1/2 sum_1..k sum_1..k (p_i)(p_j) - 3rd order term + 4th order term ...
+ * //
+ * //   p(incorrect) is sum_1..k(p_i) - 1/2 sum_1..k sum_1..k (p_i)(p_j) + 3rd order term - 4th order term ...
+ * //
+ * // =====>>>  max_1..k(p_i) < p(incorrect) < sum_1..k(p_i).
+ * //
+ * // approximate with linear terms.  (i.e. probability of 2 or more bases being called incorrectly is low).  bound is not that tight.
+ * //
+ * // =====>>>  max_1..k(p_i) < p(
+ * //
+ * // max_1..k(p_i) ~= min_1..k(q_i)
+ *
+ *
+ *  each term in (1-p_i) is (1- 10^(q/-10)) = (1 - e ^ ((log 10)(q/-10)))
+ *  accumulate using sum(log(term))., expand using e^(sum(log(term))), calculate -10 log_10(1-cumulative).
+ *
+ */
+template<typename ENCODING, typename Iterator, typename TO, int K>
+struct generate_qual
+{
+    typedef typename std::iterator_traits<Iterator>::value_type TI;
+
+    TO value;
+    TO internal;
+    TO terms[K];
+    int pos;
+    int zeroCount;
+
+    static constexpr TO logE_10 = std::log(10);
+    static_assert(std::is_floating_point<TO>::value, "generate_qual output needs to be floating point type");
+
+    generate_qual()
+        : value(1), pos(0)
+    {
+      for (int i = 0; i < K; ++i)
+      {
+        terms[i] = 0.0;
+      }
+      zeroCount = K;
+    }
+
+    TO compute(TI v)
+    {
+      if (v == 0)
+        return 0.0;   // probability of being correct
+      else
+        return 1.0 - std::exp(static_cast<TO>(v) / -10.0f * logE_10); // prob of being correct.
+    }
+
+    size_t operator()(Iterator &iter)
+    {
+
+      // drop the old value
+      TO oldval = terms[pos];
+
+      // add the new value,       // update the position  - circular queue
+      TO newval = compute(*iter - 33);  // this is for Sanger encoding.
+      terms[pos] = newval;
+      pos = (pos + 1) % K;
+
+      // save the old zero count.
+      int oldZeroCount = zeroCount;
+
+      // update the zero count
+      if (newval == 0.0)
+      {
+//        printf("ZERO!\n");
+        ++zeroCount;
+      }
+      if (oldval == 0.0)
+      {
+        --zeroCount;
+      }
+
+      // if any is zero, then return 0.
+      if (zeroCount > 0)
+      {
+        internal = 0.0;
+        value = 0.0;
+      }
+      else
+      {
+        // else there is no zero,
+
+        if (oldZeroCount == 1)
+        {
+          //printf("HAD ZEROS!\n");
+          // removed a zero.  so recalculate.
+          internal = 0.0;
+          for (int i = 0; i < K; ++i)
+          {
+            internal += std::log(terms[i]);
+          }
+        }
+        else
+        {
+          // there was not a zero.  so update.
+          internal = internal + std::log(newval) - std::log(oldval);
+        }
+
+        TO v = std::exp(internal);  // prob of being correct.
+        if (v == 1.0)
+        {
+          value = 1000;
+          printf("confident kmer!\n");
+        }
+        else
+        {
+          value = -10.0 * std::log10(1.0 - std::exp(internal));
+        }
+      }
+
+//      printf("qual %d oldval = %f, newval = %f, internal = %f, output val = %f\n", *iter, oldval, newval, internal, value);
+
+      // move the iterator.
+      ++iter;
+      return value;
+    }
+
+    TO operator()()
+    {
+      return value;
+    }
+
+};
+
+#define K 11
+
+
+typedef generate_kmers<DNA, char*, uint64_t, K> op_type;
+op_type kmer_op;
+typedef bliss::iterator::buffered_transform_iterator<op_type, char*> read_iter_type;
+
+struct SANGER
+{
+};
+typedef generate_qual<SANGER, char*, double, K> qual_op_type;
+qual_op_type qual_op;
+typedef bliss::iterator::buffered_transform_iterator<qual_op_type, char*> qual_iter_type;
+
 
 
 void fileio() {
@@ -43,16 +262,39 @@ void fileio() {
 void compute(int rank, int pid, bliss::iterator::fastq_sequence<char*> &read, int j) {
 
 
+  read_iter_type start(read.seq, kmer_op);
+  read_iter_type end(read.seq_end, kmer_op);
+
+  qual_iter_type qstart(read.qual, qual_op);
+  qual_iter_type qend(read.qual_end, qual_op);
+
+  uint64_t kmer = 0;
+  double qual = 0;
+  uint64_t kmerCount = 0;
+
+  int i = -1;
+  // NOTE: need to call *start to actually evaluate.  question is whether ++ should be doing computation.
+  for (; (start != end) && (qstart != qend); ++start, ++qstart)
+  {
+    ++i;
+
+    if (i < (K - 1))
+      continue;
+//        kmers.push_back(*start);
+    kmer ^= *start;
+    qual = *qstart - qual;
+    ++kmerCount;
+  }
 
 
+//
+//
+//  char* out = new char[128];
+//  sprintf(out, "compute %d, %ld, %d\n", pid, read.id, j);
+//  delete [] out;
 
-
-  char* out = new char[128];
-  sprintf(out, "compute %d, %ld, %d\n", pid, read.id, j);
-  delete [] out;
-
-  if (j % 10000 == 0)
-    printf("%d.%d ", rank, pid);
+//  if (j % 10000 == 0)
+//    printf("%d.%d ", rank, pid);
 
 }
 
@@ -164,10 +406,6 @@ int main(int argc, char** argv) {
 
     std::cout << rank << " record adjusted " << loader.getRange() << std::endl;
 
-    t1 = std::chrono::high_resolution_clock::now();
-    bliss::io::fastq_loader::iterator fastq_start = loader.begin();
-    bliss::io::fastq_loader::iterator fastq_end = loader.end();
-
 
 
 
@@ -179,9 +417,14 @@ int main(int argc, char** argv) {
     std::thread networkread_t(networkread);
 
     std::cout << "threads started" << std::endl;
-    // do some work using openmp
 
-    int threadcount = 3;
+    // do some work using openmp
+    t1 = std::chrono::high_resolution_clock::now();
+    bliss::io::fastq_loader::iterator fastq_start = loader.begin();
+    bliss::io::fastq_loader::iterator fastq_end = loader.end();
+
+
+    int threadcount = 4;
 
     bliss::iterator::fastq_sequence<char*> read;
     int i = 0;
@@ -198,12 +441,12 @@ int main(int argc, char** argv) {
 //            ++i;
 #pragma omp task firstprivate(read, i)
         {
-          // copy
+          // copy read.  not doing that right now.
 
           // then compute
           compute(rank, omp_get_thread_num(), read, i);
 
-          // release
+          // release resource
 
         }
 
@@ -212,8 +455,11 @@ int main(int argc, char** argv) {
       }
 #pragma omp taskwait
     }
-    std::cout << "computation done" << ". " << i << std::endl;
 
+    t2 = std::chrono::high_resolution_clock::now();
+    time_span = std::chrono::duration_cast<std::chrono::duration<double>>(
+        t2 - t1);
+    INFO("computation rank " << rank << " elapsed time: " << time_span.count() << "s.");
 
     //fileio_t.join();
     networkwrite_t.join();
