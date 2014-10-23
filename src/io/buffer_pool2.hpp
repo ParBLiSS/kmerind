4dd14f73b7d3f2bf3e2ca7b6a43edba78bd69ddc
tcpan <tpan7@gatech.edu>
2014-10-23 18:20:43 -0400

    WIP: add new buffer pool that returns unique_ptrs instead of ids.

---
+++ src/io/buffer_pool2.hpp
@@ -0,0 +1,321 @@
+/**
+ * @file		buffer_pool2.hpp
+ * @ingroup bliss::io
+ * @author	tpan
+ * @brief   this file defines in-memory Buffer Pool.
+ * @details
+ *
+ * Copyright (c) 2014 Georgia Institute of Technology.  All Rights Reserved.
+ *
+ * TODO add License
+ */
+#ifndef BUFFERPOOL2_HPP_
+#define BUFFERPOOL2_HPP_
+
+#include <cassert>
+
+#include <mutex>
+#include <deque>
+#include <stdexcept>
+
+#include "utils/logging.h"
+#include "io/buffer.hpp"
+#include "concurrent/concurrent.hpp"
+
+
+
+namespace bliss
+{
+  namespace io
+  {
+    // TODO: move constructor and assignment operators between BufferPool2s of different thread safeties.
+
+    /**
+     * @class     BufferPool2
+     * @brief     BufferPool2 manages a set of reusable, in-memory buffers.  In particular, it can limit the amount of memory usage.
+     * @details   The class is templated to provide thread-safe or unsafe BufferPool2s, managing thread-safe or unsafe Buffers
+     *
+     *            Each buffer is a block of preallocated memory that can be appended into.
+     *
+     *            The caller can acquire a new buffer from the pool and release an old buffer back into the pool, by Id.
+     *            Released buffers are marked as empty and available.
+     *
+     *            When the pool is exhausted, Acquire function returns false.
+     *
+     *      life cycle:  pool acquire():  buffer unblock().  -> allow rw.
+     *              application:  buffer block() -> allow ro
+     *              pool release():  buffer clear().
+     *
+     *
+     * @note      Each buffer should be acquired by a single thread  and released by a single thread.   (NOT REASONABLE)
+     *            Note that Buffer does not actualy clear the memory, just resets the size, which is also the pointer for insertion.
+     *            While the release method handles duplicate releases, there may be race conditions when multithreading, including
+     *              likely loss of data (thread 1 appends data and thread 2 releases  it.
+     *
+     *            the pools passes ownership of the buffers to callers.  This is so to reduce race conditions when multiple
+     *            threads are using and acquiring buffers.
+     *
+     *
+     *
+     *
+     *  @tparam PoolTS    The thread safety property for the pool
+     *  @tparam BufferTS  The thread safety property of each Buffer.
+     */
+    template<bliss::concurrent::ThreadSafety PoolTS, bliss::concurrent::ThreadSafety BufferTS = PoolTS>
+    class BufferPool2
+    {
+      public:
+        /**
+         * @brief     Type of Buffer in the BufferPool2 (thread safe or not)
+         */
+        using BufferType = bliss::io::Buffer<BufferTS>;
+        /**
+         * @brief     Index Type used to reference the Buffers in the BufferPool2.
+         */
+        using BufferPtrType = std::unique_ptr<BufferType>;
+
+      protected:
+        /**
+         * @brief     capacity of the BufferPool2 (in number of Buffers)
+         */
+        mutable size_t capacity;
+
+        /**
+         * @brief     capacity of the individual Buffers (in bytes)
+         */
+        const size_t buffer_capacity;
+
+        /**
+         * @brief     current pool size
+         */
+        mutable std::atomic<size_t> instanceCount;
+
+        /**
+         * @brief     Internal Set of available Buffers for immediate use.
+         * @details   Using set instead of ThreadSafeQueue to ensure uniqueness of Buffer Ids.
+         */
+        std::deque<BufferPtrType>                          available;
+
+        /**
+         * @brief     mutex to control access.
+         */
+        mutable std::mutex mutex;
+
+
+        /**
+         * @brief     Private move constructor with mutex lock.
+         * @param other   Source BufferPool2 object to move
+         * @param l       Mutex lock on the Source BufferPool2 object
+         */
+        BufferPool2(BufferPool2<PoolTS, BufferTS>&& other, const std::lock_guard<std::mutex>& l) :
+          capacity(other.capacity),
+          buffer_capacity(other.buffer_capacity),
+          instanceCount(other.instanceCount),
+          available(std::move(other.available)) {
+
+          other.capacity = 0;
+          other.instanceCount = 0;
+          other.available.clear();
+        };
+
+
+      public:
+        /**
+         * @brief     construct a Buffer Pool with buffers of capacity _buffer_capacity.  the number of buffers in the pool is set to _pool_capacity.
+         *
+         * @param _pool_capacity       number of buffers in the pool
+         * @param _buffer_capacity     size of the individual buffers
+         */
+        BufferPool2(const size_t _pool_capacity, const size_t _buffer_capacity) :
+          capacity(_pool_capacity),
+          buffer_capacity(_buffer_capacity),
+          instanceCount(0), available()
+          {
+
+          // get an estimated size first, so don't have to keep growing the vector
+          instanceCount.store(isFixedSize() ? capacity : 4, std::memory_order_release);
+
+          //DEBUGF("RESERVING: %d for buffer pool", size_hint);
+          for (size_t i = 0; i < instanceCount.load(std::memory_order_consume); ++i) {
+            BufferPtrType ptr(new BufferType(buffer_capacity));
+            ptr->block();
+            ptr->clear();
+            available.push_back(std::move(ptr));
+          }
+        };
+
+        /**
+         * @brief     default constructor is deleted.
+         */
+        BufferPool2() = delete;
+
+        /**
+         * @brief     construct a Buffer pool with buffers of capacity _buffer_capacity.  the number of buffers in the pool is unbounded.
+         *
+         * @param _buffer_capacity    size of the individual buffers
+         */
+        explicit BufferPool2(const size_t _buffer_capacity) :
+            BufferPool2<PoolTS, BufferTS>(std::numeric_limits<size_t>::max(), _buffer_capacity) {};
+
+        /**
+         * @brief     default copy constructor is deleted.
+         * @param other   source BufferPool2 object to copy from.
+         */
+        explicit BufferPool2(const BufferPool2<PoolTS, BufferTS>& other) = delete;
+
+        /**
+         * @brief      Move constructor.  Delegates to the Move constructor that locks access on the source.
+         * @param other    source BufferPool2 object to move.
+         */
+        explicit BufferPool2(BufferPool2<PoolTS, BufferTS>&& other) :
+          BufferPool2<PoolTS, BufferTS>(std::move(other), std::lock_guard<std::mutex>(other.mutex)) {};
+
+        /**
+         * @brief     default copy assignment operator is deleted.
+         * @param other   source BufferPool2 object to copy from.
+         * @return        self, with member variables copied from other.
+         */
+        BufferPool2<PoolTS, BufferTS>& operator=(const BufferPool2<PoolTS, BufferTS>& other) = delete;
+
+        /**
+         * @brief     move assignment operator.
+         * @param other   source BufferPool2 object to move from.
+         * @return        self, with member variables moved from other.
+         */
+        BufferPool2<PoolTS, BufferTS>& operator=(BufferPool2<PoolTS, BufferTS>&& other) {
+          std::unique_lock<std::mutex> mylock(mutex, std::defer_lock),
+                                        otherlock(other.mutex, std::defer_lock);
+          std::lock(mylock, otherlock);
+
+          capacity = other.capacity; other.capacity = 0;
+          buffer_capacity = other.buffer_capacity; other.buffer_capacity = 0;
+          instanceCount = other.instanceCount; other.instanceCount = 0;
+          available = std::move(other.available);
+          return *this;
+        };
+
+
+        /**
+         * @brief     default destructor
+         */
+        virtual ~BufferPool2() {};
+
+        /**
+         * @brief     Current size of the BufferPool2
+         * @return  size, type IdType (aka int).
+         */
+        const size_t getSize() const  {
+          return instanceCount.load(std::memory_order_consume);
+        }
+
+        /**
+         * @brief     Current size of the BufferPool2
+         * @return  size, type IdType (aka int).
+         */
+        const size_t getAvailableCount() const  {
+          std::lock_guard lock(mutex);
+          return available.size();
+        }
+
+        /**
+         * @brief     Current capacity of the BufferPool2
+         * @return    capacity, type IdTyype (aka int)
+         */
+        const size_t getCapacity() const {
+          return capacity;
+        }
+
+        /**
+         * @brief     Each buffer's maximum capacity.
+         * @return  each buffer's maximum capacity.
+         */
+        const size_t getBufferCapacity() const {
+          return buffer_capacity;
+        }
+
+        /**
+         * @brief     whether the BufferPool2 is growable
+         * @return    bool indicating if BufferPool2 is growable.
+         */
+        inline const bool isFixedSize() const {
+          return capacity < std::numeric_limits<size_t>::max();
+        }
+
+        /**
+         * @brief     Resets the entire BufferPool2: all Buffers in pool are  marked as released and available.
+         *
+         * @note      This is not entirely thread safe.  the available set is cleared by a single thread, but other
+         *            threads may be acquiring buffers while they are being released.
+         *
+         *            It is envisioned that this function should be called from a single thread.
+         *            however, care must be taken to ensure that all other threads have completed their work, else data loss is likely.
+         */
+        void reset() {
+          std::lock_guard<std::mutex> lock(mutex);
+          instanceCount.store(available.size(), std::memory_order_acq_rel);
+        }
+
+
+        /**
+         * @brief     Get the next available Buffer by id.  if none available, return false in the first argument of the pair.
+         * @return    std::pair with bool and Id,  first indicate whether acquire was successful, second indicate the BufferId if successful.
+         */
+        BufferPtrType tryAcquireBuffer() {
+          BufferPtrType ptr;  // default is a null ptr.
+
+          std::lock_guard lock(mutex);
+          if (available.empty()) {
+            // none available for reuse
+            if (instanceCount.load(std::memory_order_consume) < capacity) {
+              // but has room to allocate, so do it.
+              instanceCount.fetch_add(1, std::memory_order_acq_rel);
+              ptr = std::move(BufferPtrType(new BufferType(buffer_capacity)));
+              ptr->clear();
+              ptr->unblock();
+            }  // else already at max.  so no new allocation.
+          } else {
+            // has available for reuse.
+            ptr = std::move(available.front());
+            available.pop_front();
+            ptr->clear();
+            ptr->unblock();
+          }
+
+          return std::move(ptr);
+        }
+
+        /**
+         * @brief     Release a buffer back to pool, by id.  if id is incorrect, throw std exception.  clears buffer before release back into available.
+         * @note      THREAD SAFE
+         * @param bufferId    The id of the Buffer to be released.
+         */
+        template<bliss::concurrent::ThreadSafety TS = PoolTS>
+        typename std::enable_if<TS == bliss::concurrent::THREAD_SAFE, void>::type releaseBuffer(BufferPtrType&& ptr) {
+          std::lock_guard<std::mutex> lock(mutex);
+          assert(ptr->isBlocked());
+          ptr->clear();
+          available.push_back(std::move(ptr));
+        }
+
+
+        /**
+         * @brief     Release a buffer back to pool, by id.  if id is incorrect, throw std exception.  clears buffer before release back into available.
+         * @note      THREAD UNSAFE
+         * @param bufferId    The id of the Buffer to be released.
+         */
+        template<bliss::concurrent::ThreadSafety TS = PoolTS>
+        typename std::enable_if<TS == bliss::concurrent::THREAD_UNSAFE, void>::type releaseBuffer(BufferPtrType&& ptr) {
+          assert(ptr->isBlocked());
+          ptr->clear();
+          available.push_back(std::move(ptr));
+
+          //DEBUGF("buffer id %d is available for reuse.", id);
+        }
+
+    };
+
+
+  } /* namespace io */
+} /* namespace bliss */
+
+#endif /* BUFFERPOOL2_HPP_ */
---
+++ src/io/test/check_bufferpool2.cpp
@@ -0,0 +1,247 @@
+/**
+ * @file		check_bufferpool2.cpp
+ * @ingroup
+ * @author	tpan
+ * @brief
+ * @details
+ *
+ * Copyright (c) 2014 Georgia Institute of Technology.  All Rights Reserved.
+ *
+ * TODO add License
+ */
+
+#include <unistd.h>  // for usleep
+
+#include "io/buffer_pool2.hpp"
+#include "omp.h"
+#include <cassert>
+#include <chrono>
+#include <cstdlib>   // for rand
+
+template<typename PoolType>
+void testPool(PoolType && pool, const std::string &name, int pool_threads, int buffer_threads) {
+
+  printf("TESTING %s %s: pool threads %d, buffer threads %d\n", name.c_str(), (pool.isFixedSize() ? "FIXED" : "GROW"),  pool_threads, buffer_threads);
+
+  printf("TEST acquire\n");
+  int expected;
+  int i = 0;
+  typename PoolType::IdType id = 0;
+  int count = 0;
+#pragma omp parallel for num_threads(pool_threads) default(none) private(i, id) shared(pool) reduction(+ : count)
+  for (i = 0; i < 100; ++i) {
+    if (!pool.tryAcquireBuffer().first) {
+      ++count;
+    }
+  }
+  expected = pool.getCapacity();
+  expected = std::max(0, 100 - expected);
+  if (count != expected) printf("ERROR: number of failed attempt to acquire buffer should be %d, actual %d.  pool capacity %d, size: %d \n", expected, count, pool.getCapacity(), pool.getSize());
+  pool.reset();
+
+  printf("TEST acquire with growth\n");
+  id = 0;
+  i = 0;
+  count = 0;
+#pragma omp parallel for num_threads(pool_threads) default(none) private(i, id) shared(pool) reduction(+ : count)
+  for (i = 0; i < 150; ++i) {
+    if (!pool.tryAcquireBuffer().first) {
+      ++count;
+    }
+  }
+  expected = pool.getCapacity();
+  expected = std::max(0, 150 - expected);
+  if (count != expected) printf("ERROR: number of failed attempt to acquire buffer should be %d, actual %d.  pool size: %d \n", expected, count, pool.getSize());
+
+
+
+  printf("TEST release\n");
+  count = 0;
+  id = 0;
+#pragma omp parallel for num_threads(pool_threads) default(none) private(id) shared(pool) reduction(+ : count)
+  for (id = 0; id < 350; ++id) {
+    try {
+      pool[id].block();
+      pool.releaseBuffer(id);
+    } catch (const std::out_of_range & e)
+    {
+      ++count;
+    }
+  }
+  expected = 350 - pool.getSize();
+  if (count != expected) printf("ERROR: number of failed attempt to release buffer should be %d, actual %d. pool size: %d \n", expected, count, pool.getSize());
+
+#pragma omp parallel for num_threads(pool_threads) default(none) private(id) shared(pool) reduction(+ : count)
+  for (id = 0; id < pool.getSize(); ++id) {
+    pool[id].unblock();
+  }
+
+  printf("TEST access by multiple threads, each a separate buffer.\n");
+#pragma omp parallel num_threads(pool_threads) default(none) shared(pool)
+  {
+    int v = omp_get_thread_num() + 5;
+    pool[omp_get_thread_num()+ 1].append(&v, sizeof(int));
+  }
+#pragma omp parallel num_threads(pool_threads) default(none) shared(pool)
+  {
+    int u = reinterpret_cast<const int*>(pool[omp_get_thread_num() + 1].getData())[0];
+    if (omp_get_thread_num() + 5 != u) printf("ERROR: %d value inserted was %d, getting %d\n", omp_get_thread_num(), omp_get_thread_num() + 5, u);
+  }
+  pool.reset();
+
+  printf("TEST access by multiple threads, all to same buffer.\n");
+#pragma omp parallel num_threads(buffer_threads) default(none) shared(pool)
+  {
+    int v = omp_get_thread_num() + 7;
+    pool[1].append(&v, sizeof(int));
+  }
+  printf("values inserted were ");
+  for (int i = 0; i <= buffer_threads ; ++i) {
+    int u = reinterpret_cast<const int*>(pool[1].getData())[i];
+    printf(", %d", u);
+  }
+  printf("\n");
+
+  pool.reset();
+  omp_set_nested(1);
+
+  printf("TEST all operations together\n");
+#pragma omp parallel num_threads(pool_threads) default(none) shared(pool, pool_threads, buffer_threads)
+  {
+    // Id range is 0 to 100
+    typename PoolType::IdType id;
+    int iter;
+    int j = 0;
+    for (int i = 0; i < 100; ++i) {
+      // acquire
+      auto newBuf = pool.tryAcquireBuffer();
+      while (!newBuf.first) usleep(50);
+      //if (i % 25 == 0)
+//      printf("thread %d acquired buffer %d\n", omp_get_thread_num(), id);
+      id = newBuf.second;
+      // access
+      iter = rand() % 100;
+#pragma omp parallel for num_threads(buffer_threads) default(none) shared(pool, id, iter) private(j)
+      for (j = 0; j < iter; ++j) {
+        pool[id].append(&j, sizeof(int));
+      }
+
+      // random sleep
+      usleep(rand() % 1000);
+      if (pool[id].getSize() != sizeof(int) * iter)
+        printf("ERROR: thread %d/%d buffer %d size is %lu, expected %lu\n", omp_get_thread_num(), pool_threads, id, pool[id].getSize(), sizeof(int) * iter);
+
+      // clear buffer
+      pool[id].block();
+      pool[id].clear();
+
+      //release
+      pool.releaseBuffer(id);
+      //if (i % 25 == 0)
+//      printf("thread %d released buffer %d\n", omp_get_thread_num(), id);
+
+    }
+  }
+
+
+
+
+};
+
+
+int main(int argc, char** argv) {
+
+  // construct, acquire, access, release
+
+  //////////////  unbounded version
+
+  /// thread unsafe.  test in single thread way.
+//  bliss::io::BufferPool2<bliss::concurrent::THREAD_UNSAFE> usPool(8192);
+
+
+  testPool(std::move(bliss::io::BufferPool2<bliss::concurrent::THREAD_UNSAFE>(8192)), "thread unsafe pool", 1, 1);
+
+
+
+
+  testPool(std::move(bliss::io::BufferPool2<bliss::concurrent::THREAD_SAFE>(8192)), "thread safe pool, thread safe buffer", 1, 1);
+  testPool(std::move(bliss::io::BufferPool2<bliss::concurrent::THREAD_SAFE>(8192)), "thread safe pool, thread safe buffer", 1, 2);
+  testPool(std::move(bliss::io::BufferPool2<bliss::concurrent::THREAD_SAFE>(8192)), "thread safe pool, thread safe buffer", 1, 3);
+  testPool(std::move(bliss::io::BufferPool2<bliss::concurrent::THREAD_SAFE>(8192)), "thread safe pool, thread safe buffer", 1, 4);
+  testPool(std::move(bliss::io::BufferPool2<bliss::concurrent::THREAD_SAFE>(8192)), "thread safe pool, thread safe buffer", 1, 8);
+
+  testPool(std::move(bliss::io::BufferPool2<bliss::concurrent::THREAD_SAFE>(8192)), "thread safe pool, thread safe buffer", 2, 1);
+  testPool(std::move(bliss::io::BufferPool2<bliss::concurrent::THREAD_SAFE>(8192)), "thread safe pool, thread safe buffer", 2, 2);
+  testPool(std::move(bliss::io::BufferPool2<bliss::concurrent::THREAD_SAFE>(8192)), "thread safe pool, thread safe buffer", 2, 3);
+  testPool(std::move(bliss::io::BufferPool2<bliss::concurrent::THREAD_SAFE>(8192)), "thread safe pool, thread safe buffer", 2, 4);
+
+  testPool(std::move(bliss::io::BufferPool2<bliss::concurrent::THREAD_SAFE>(8192)), "thread safe pool, thread safe buffer", 3, 1);
+  testPool(std::move(bliss::io::BufferPool2<bliss::concurrent::THREAD_SAFE>(8192)), "thread safe pool, thread safe buffer", 3, 2);
+  testPool(std::move(bliss::io::BufferPool2<bliss::concurrent::THREAD_SAFE>(8192)), "thread safe pool, thread safe buffer", 3, 3);
+
+
+
+
+
+
+  testPool(std::move(bliss::io::BufferPool2<bliss::concurrent::THREAD_SAFE, bliss::concurrent::THREAD_UNSAFE>(8192)), "thread safe pool, thread unsafe buffer", 1, 1);
+  testPool(std::move(bliss::io::BufferPool2<bliss::concurrent::THREAD_SAFE, bliss::concurrent::THREAD_UNSAFE>(8192)), "thread safe pool, thread unsafe buffer", 2, 1);
+  testPool(std::move(bliss::io::BufferPool2<bliss::concurrent::THREAD_SAFE, bliss::concurrent::THREAD_UNSAFE>(8192)), "thread safe pool, thread unsafe buffer", 3, 1);
+  testPool(std::move(bliss::io::BufferPool2<bliss::concurrent::THREAD_SAFE, bliss::concurrent::THREAD_UNSAFE>(8192)), "thread safe pool, thread unsafe buffer", 4, 1);
+
+
+
+
+  // this one is not defined because it's not logical.  not compilable.
+  // bliss::io::BufferPool2<bliss::concurrent::THREAD_UNSAFE, bliss::concurrent::THREAD_SAFE> tsusPool(8192, 8);
+
+
+
+
+  /////////////  fixed size version.
+
+
+
+  /// testing buffer_pool. thread unsafe
+
+
+  testPool(std::move(bliss::io::BufferPool2<bliss::concurrent::THREAD_UNSAFE>(8, 8192)), "thread unsafe pool", 1, 1);
+
+
+
+
+  /// testing buffer_pool. thread safe
+
+  testPool(std::move(bliss::io::BufferPool2<bliss::concurrent::THREAD_SAFE>(8, 8192)), "thread safe pool, thread safe buffer", 1, 1);
+  testPool(std::move(bliss::io::BufferPool2<bliss::concurrent::THREAD_SAFE>(8, 8192)), "thread safe pool, thread safe buffer", 1, 2);
+  testPool(std::move(bliss::io::BufferPool2<bliss::concurrent::THREAD_SAFE>(8, 8192)), "thread safe pool, thread safe buffer", 1, 3);
+  testPool(std::move(bliss::io::BufferPool2<bliss::concurrent::THREAD_SAFE>(8, 8192)), "thread safe pool, thread safe buffer", 1, 4);
+  testPool(std::move(bliss::io::BufferPool2<bliss::concurrent::THREAD_SAFE>(8, 8192)), "thread safe pool, thread safe buffer", 1, 8);
+
+  testPool(std::move(bliss::io::BufferPool2<bliss::concurrent::THREAD_SAFE>(8, 8192)), "thread safe pool, thread safe buffer", 2, 1);
+  testPool(std::move(bliss::io::BufferPool2<bliss::concurrent::THREAD_SAFE>(8, 8192)), "thread safe pool, thread safe buffer", 2, 2);
+  testPool(std::move(bliss::io::BufferPool2<bliss::concurrent::THREAD_SAFE>(8, 8192)), "thread safe pool, thread safe buffer", 2, 3);
+  testPool(std::move(bliss::io::BufferPool2<bliss::concurrent::THREAD_SAFE>(8, 8192)), "thread safe pool, thread safe buffer", 2, 4);
+
+  testPool(std::move(bliss::io::BufferPool2<bliss::concurrent::THREAD_SAFE>(8, 8192)), "thread safe pool, thread safe buffer", 3, 1);
+  testPool(std::move(bliss::io::BufferPool2<bliss::concurrent::THREAD_SAFE>(8, 8192)), "thread safe pool, thread safe buffer", 3, 2);
+  testPool(std::move(bliss::io::BufferPool2<bliss::concurrent::THREAD_SAFE>(8, 8192)), "thread safe pool, thread safe buffer", 3, 3);
+
+
+
+  /// testing buffer_pool, thread safe pool, unsafe buffers.
+
+  testPool(std::move(bliss::io::BufferPool2<bliss::concurrent::THREAD_SAFE, bliss::concurrent::THREAD_UNSAFE>(8, 8192)), "thread safe pool, thread unsafe buffer", 1, 1);
+  testPool(std::move(bliss::io::BufferPool2<bliss::concurrent::THREAD_SAFE, bliss::concurrent::THREAD_UNSAFE>(8, 8192)), "thread safe pool, thread unsafe buffer", 2, 1);
+  testPool(std::move(bliss::io::BufferPool2<bliss::concurrent::THREAD_SAFE, bliss::concurrent::THREAD_UNSAFE>(8, 8192)), "thread safe pool, thread unsafe buffer", 3, 1);
+  testPool(std::move(bliss::io::BufferPool2<bliss::concurrent::THREAD_SAFE, bliss::concurrent::THREAD_UNSAFE>(8, 8192)), "thread safe pool, thread unsafe buffer", 4, 1);
+
+
+  // this one is not defined because it's not logical.  not compilable.
+  // bliss::io::BufferPool2<bliss::concurrent::THREAD_UNSAFE, bliss::concurrent::THREAD_SAFE> tsusPool(8, 8192);
+
+
+
+
+
+}
