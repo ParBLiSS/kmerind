b00ea65c1f48e7455050c58cce994087da633b67
tcpan <tpan7@gatech.edu>
2015-08-03 10:49:31 -0400

    ADD: filter iterator test.

---
+++ src/iterators/test/test_filter_iterator.cpp
@@ -0,0 +1,111 @@
+/**
+ * test_filter_iterator.cpp
+ * Test filter_iterator class
+ *  Created on: Aug 27, 2014
+ *      Author: Tony Pan <tpan7@gatech.edu>
+ */
+
+#include "iterators/filter_iterator.hpp"
+#include "iterators/counting_iterator.hpp"
+
+#include <gtest/gtest.h>
+#include <cstdint>  // for uint64_t, etc.
+#include <limits>
+
+using namespace bliss::iterator;
+
+/*
+ * test class holding some information.  Also, needed for the typed tests
+ */
+template<typename T>
+class FilterIteratorTest : public ::testing::Test
+{
+    static_assert(std::is_integral<T>::value, "data type has to be integral.");
+
+  protected:
+
+    struct Even {
+      bool operator()(T const & v) {
+        return (v % 2) == 0;
+      }
+    };
+
+    struct Odd {
+      bool operator()(T const & v) {
+        return (v % 2) == 1;
+      }
+    };
+
+    CountingIterator<T> base_iter_begin;
+    CountingIterator<T> base_iter_end;
+    filter_iterator<Even, CountingIterator<T> > even_filter_begin;
+    filter_iterator<Even, CountingIterator<T> > even_filter_end;
+    filter_iterator<Odd, CountingIterator<T> > odd_filter_begin;
+    filter_iterator<Odd, CountingIterator<T> > odd_filter_end;
+
+    virtual void SetUp()
+    {
+      base_iter_begin = CountingIterator<T>(0, 1);
+      base_iter_end = CountingIterator<T>(1000, 1);
+      even_filter_begin = filter_iterator<Even, CountingIterator<T> >(Even(), base_iter_begin, base_iter_end);
+      even_filter_end = filter_iterator<Even, CountingIterator<T> >(Even(), base_iter_end, base_iter_begin);
+      odd_filter_begin = filter_iterator<Odd, CountingIterator<T> >(Odd(), base_iter_begin, base_iter_end);
+      odd_filter_end = filter_iterator<Odd, CountingIterator<T> >(Odd(), base_iter_end);
+    }
+
+};
+
+// indicate this is a typed test
+TYPED_TEST_CASE_P(FilterIteratorTest);
+
+
+
+// testing the copy constructor
+TYPED_TEST_P(FilterIteratorTest, increment){
+
+  int i = 0;
+  auto it = this->even_filter_begin;
+  for (; it != this->even_filter_end; ++it, i += 2) {
+    ASSERT_TRUE(*it % 2 == 0);
+    ASSERT_EQ(*it, i);
+  }
+
+  // decrement
+  --it;
+  i -= 2;
+  for (; it != this->even_filter_begin; --it, i -= 2) {
+    ASSERT_TRUE(*it % 2 == 0);
+    ASSERT_EQ(*it, i);
+  }
+
+
+  i = 1;
+  auto it2 = this->odd_filter_begin;
+  for (; it2 != this->odd_filter_end; ++it2, i += 2) {
+    ASSERT_TRUE(*it2 % 2 == 1);
+    ASSERT_EQ(*it2, i);
+  }
+
+  --it2;
+  i -= 2;
+  for (; it2 != this->odd_filter_begin; --it2, i -= 2) {
+    ASSERT_TRUE(*it2 % 2 == 1);
+    ASSERT_EQ(*it2, i);
+  }
+
+
+}
+
+
+
+
+
+
+// now register the test cases
+REGISTER_TYPED_TEST_CASE_P(FilterIteratorTest, increment);
+
+
+//////////////////// RUN the tests with different types.
+
+typedef ::testing::Types<int32_t, uint32_t> FilterIteratorTestTypes;
+INSTANTIATE_TYPED_TEST_CASE_P(Bliss, FilterIteratorTest, FilterIteratorTestTypes);
