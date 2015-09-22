be660c9966f2a30f9f7611163883de846ba43543
tcpan <tpan7@gatech.edu>
2015-09-22 00:18:28 -0400

    FIX: removed nonexistent dependency, and fix GCCFilter incorrect var name

--- cmake/GCCFilter.cmake
+++ cmake/GCCFilter.cmake
@@ -1,17 +1,18 @@
 ###### GCCFilt support, using my fixed version of gccfilter.
 OPTION(ENABLE_GCCFILTER "Use GCCFilter for prettifying compiler output.  Requires Perl" OFF)
 
-CMAKE_DEPENDENT_OPTION(GCCFILT_IDE_COMPAT "If compiling from an IDE, turn off colorization for compatibility" OFF "ENABLE_GCCFILTER" OFF)
+CMAKE_DEPENDENT_OPTION(GCCFILTER_IDE_COMPAT "If compiling from an IDE, turn off colorization for compatibility" OFF "ENABLE_GCCFILTER" OFF)
 
 if (ENABLE_GCCFILTER)
 
 if (GCCFILTER_IDE_COMPAT)
     set(GCCFILTER_COLORIZE "" CACHE INTERNAL "gccfilter flag for colorization" FORCE)
-else ()
+else (GCCFILTER_IDE_COMPAT)
     set(GCCFILTER_COLORIZE "-c" CACHE INTERNAL "gccfilter flag for colorization" FORCE)
-endif()
+endif(GCCFILTER_IDE_COMPAT)
 
 SET(GCCFILTER_VERBOSITY 0 CACHE STRING "G++ STL message condensation level:  -1, 0, 1, 2, 3, 4.  0 is most terse, 4 is most verbose, -1 is unmodified.")
+
 if("${GCCFILTER_VERBOSITY}" STREQUAL "4")
     set_property(GLOBAL PROPERTY RULE_MESSAGES OFF)
     set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE "${CMAKE_SOURCE_DIR}/utils/gccfilter/gccfilter ${GCCFILTER_COLORIZE}")
