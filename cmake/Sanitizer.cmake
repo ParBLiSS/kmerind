6a9d9ea13bb3163b50bdc82ad93cf66242697beb
tcpan <tpan7@gatech.edu>
2015-10-03 16:48:00 -0400

    ENH: clang support for sanitizer.  NOTE: clang 3.7 needs to be built using CMake to get the asan library.  Autotool built does not provide it.  unfortunately, default ubuntu package does not use Cmake build.

--- cmake/Sanitizer.cmake
+++ cmake/Sanitizer.cmake
@@ -4,10 +4,21 @@ if (ENABLE_SANITIZER)
   # probably should be specific based on sanitizer style.  no-omit-frame-pointer is for address.
   add_definitions(-fsanitize=${SANITIZER_STYLE} -fPIE -fno-omit-frame-pointer)
   # and static-libtsan is for thread.
-  set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fsanitize=${SANITIZER_STYLE} -fno-omit-frame-pointer -pie -static-libtsan")
-  set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} -fsanitize=${SANITIZER_STYLE} -fno-omit-frame-pointer -pie -static-libtsan")
-  set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -fsanitize=${SANITIZER_STYLE} -fno-omit-frame-pointer -pie -static-libtsan")
-  set(CMAKE_STATIC_LINKER_FLAGS "${CMAKE_STATIC_LINKER_FLAGS} -fsanitize=${SANITIZER_STYLE} -fno-omit-frame-pointer -pie -static-libtsan")
+  
+if ("${CMAKE_CXX_COMPILER_ID}" MATCHES "GNU")
+    if ("${SANITIZER_STYLE}" MATCHES "thread")
+		set(SANITIZER_EXTRA_FLAGS "-static-libtsan")
+	else()
+	    set(SANITIZER_EXTRA_FLAGS "")
+	endif()
+else()
+	set(SANITIZER_EXTRA_FLAGS "")	
+endif()
+  
+  set(CMAKE_EXE_LINKER_FLAGS    "${CMAKE_EXE_LINKER_FLAGS}    -fsanitize=${SANITIZER_STYLE} -fno-omit-frame-pointer -pie ${SANITIZER_EXTRA_FLAGS}")
+  set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} -fsanitize=${SANITIZER_STYLE} -fno-omit-frame-pointer -pie ${SANITIZER_EXTRA_FLAGS}")
+  set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -fsanitize=${SANITIZER_STYLE} -fno-omit-frame-pointer -pie ${SANITIZER_EXTRA_FLAGS}")
+  set(CMAKE_STATIC_LINKER_FLAGS "${CMAKE_STATIC_LINKER_FLAGS} -fsanitize=${SANITIZER_STYLE} -fno-omit-frame-pointer -pie ${SANITIZER_EXTRA_FLAGS}")
 
   # NOTE: if using thread sanitizer, please use g++ 4.9 and later, compiled with --disable-linux-futex (for libgomp)
   #  else lots of false positives from OMP will be reported by tsan
