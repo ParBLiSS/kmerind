82809fa79bdd3777c5674cdbb719a0dc2d75f27d
tcpan <tpan7@gatech.edu>
2016-01-06 11:28:55 -0500

    ENH: changed text inspector to be more scripting friendly.  FIX: make text inspector 64 bit compatible.

--- utils/text_inspector.cpp
+++ utils/text_inspector.cpp
@@ -14,7 +14,7 @@
 #include <string>
 #include <iostream>
 #include <iterator>  // for ostream_iterator
-
+#include <cstdlib>   // for strtoul
 
 #include <fcntl.h>      // for open
 #include <sys/stat.h>   // block size.
@@ -29,7 +29,7 @@
 
 int main(int argc, char** argv) {
 
-  assert(argc > 0);
+  assert(argc > 1);
 
   std::string filename;
   filename.assign(argv[1]);
@@ -46,6 +46,7 @@ int main(int argc, char** argv) {
   size_t file_size = static_cast<size_t>(filestat.st_size);
 
 
+
   /// open the file and get a handle.
   int file_handle = open(filename.c_str(), O_RDONLY);
   if (file_handle == -1)
@@ -53,37 +54,37 @@ int main(int argc, char** argv) {
     int myerr = errno;
     BL_FATAL("ERROR in file open: ["  << filename << "] error " << myerr << ": " << strerror(myerr));
     USED_BY_LOGGER_ONLY(myerr);
-
   }
 
+  size_t pos = 0;
+  size_t end_pos = file_size;
+
+  if (argc > 2) {
+    pos = ::std::strtoul(argv[2], NULL, 0);
+    pos = ::std::min(pos, file_size);
+  }
+  if (argc > 3) {
+    end_pos = ::std::strtoul(argv[3], NULL, 0);
+    end_pos = ::std::min(end_pos, file_size);
+  }
+
+  size_t map_size = end_pos - pos;
+
+  size_t page_size = sysconf(_SC_PAGE_SIZE);
+  size_t map_offset = pos % page_size;
+  size_t map_pos = pos - map_offset;
 
   // mmap
-  unsigned char* data = (unsigned char*)mmap64(nullptr, file_size,
+  unsigned char* data = (unsigned char*)mmap64(nullptr, end_pos - map_pos,
                                        PROT_READ,
-                                       MAP_PRIVATE | MAP_POPULATE, file_handle,
-                                       0);
+                                       MAP_PRIVATE, file_handle,
+                                       map_pos);
 
-
-
-  std::cout << "please enter start and end positions (separated by space).  negative numbers to exit exit." << std::endl;
-  ///
-  int pos = 0;
-  int endpos = 0;
-  do {
-    pos = -1;
-    endpos = -1;
-    auto rr = scanf("%d %d", &pos, &endpos);
-    BLISS_UNUSED(rr);
-    if (pos >= 0 && endpos >= 0) {
-      std::cout << "char at pos " << pos << " - " << endpos << std::endl;
-      std::ostream_iterator<unsigned char> oit(std::cout);
-      std::copy(data + pos, data + endpos, oit);
-      std::cout << std::endl;
-    }
-  } while (pos >= 0 && endpos >= 0);
+  std::ostream_iterator<unsigned char> oit(std::cout);
+  std::copy(data + map_offset, data + map_offset + map_size, oit);
+  std::cout << std::endl;
 
   ///
-  munmap(data, file_size);
-
+  munmap(data, end_pos - map_pos);
 
 }
