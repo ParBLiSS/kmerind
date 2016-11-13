b6185ef81f9461a500cac3511335db83b4b87349
tcpan <tpan7@gatech.edu>
2016-11-12 19:06:34 -0500

    ENH: reduce reserved size during parsing

--- src/io/kmer_file_helper.hpp
+++ src/io/kmer_file_helper.hpp
@@ -268,7 +268,7 @@ struct KmerFileHelper {
         std::tie(record_size, seq_len) = seq_parser.get_record_size(partition.cbegin(), partition.parent_range_bytes, partition.getRange(), partition.getRange(), 10);
         size_t est_size = (record_size == 0) ? 0 : (partition.getRange().size() + record_size - 1) / record_size;  // number of records
         est_size *= (seq_len < kmer_size) ? 0 : (seq_len - kmer_size + 1) ;  // number of kmers in a record
-        result.reserve(result.size() + est_size + (est_size >> 3));
+        result.reserve(result.size() + est_size / 2);
         BL_BENCH_END(file, "reserve", est_size);
 
         BL_BENCH_START(file);
@@ -311,7 +311,7 @@ struct KmerFileHelper {
         std::tie(record_size, seq_len) = seq_parser.get_record_size(partition.cbegin(), partition.parent_range_bytes, partition.getRange(), partition.getRange(), 10);
         size_t est_size = (record_size == 0) ? 0 : (partition.getRange().size() + record_size - 1) / record_size;  // number of records
         est_size *= (seq_len < kmer_size) ? 0 : (seq_len - kmer_size + 1) ;  // number of kmers in a record
-        result.reserve(result.size() + est_size + (est_size >> 3));
+        result.reserve(result.size() + est_size / 2 );
         BL_BENCH_END(file, "reserve", est_size);
 
         BL_BENCH_START(file);
@@ -460,7 +460,7 @@ struct KmerFileHelper {
         std::tie(record_size, seq_len) = seq_parser.get_record_size(partition.cbegin(), partition.parent_range_bytes, partition.getRange(), partition.getRange(), _comm, 10);
         size_t est_size = (record_size == 0) ? 0 : (partition.getRange().size() + record_size - 1) / record_size;  // number of records
         est_size *= (seq_len < kmer_size) ? 0 : (seq_len - kmer_size + 1) ;  // number of kmers in a record
-        result.reserve(result.size() + est_size);
+        result.reserve(result.size() + est_size / 2);
         BL_BENCH_END(file, "reserve", est_size);
 
         BL_BENCH_START(file);
@@ -506,7 +506,7 @@ struct KmerFileHelper {
         std::tie(record_size, seq_len) = seq_parser.get_record_size(partition.cbegin(), partition.parent_range_bytes, partition.getRange(), partition.getRange(), _comm, 10);
         size_t est_size = (record_size == 0) ? 0 : (partition.getRange().size() + record_size - 1) / record_size;  // number of records
         est_size *= (seq_len < kmer_size) ? 0 : (seq_len - kmer_size + 1) ;  // number of kmers in a record
-        result.reserve(result.size() + est_size);
+        result.reserve(result.size() + est_size / 2);
         BL_BENCH_END(file, "reserve", est_size);
 
         BL_BENCH_START(file);
